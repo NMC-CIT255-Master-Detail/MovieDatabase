d5595024466b9b7120a5541c77854d51339f297d
petersteele111 <petersteele111@gmail.com>
2020-10-03 16:21:31 -0400

    Add Producer View and Resource Dictionary

---
+++ MovieDatabase.WPF/Peter/Controls/ProducerAddEditUserControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MovieDatabase.WPF.Peter.Controls
+{
+    /// <summary>
+    /// Interaction logic for ProducerAddEditUserControl.xaml
+    /// </summary>
+    public partial class ProducerAddEditUserControl : UserControl
+    {
+        public ProducerAddEditUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
