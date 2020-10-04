e27cbf97244b55acdea3c7b67289461bc2053d93
petersteele111 <petersteele111@gmail.com>
2020-10-04 02:57:58 -0400

    Added Files

---
+++ MovieDatabase.WPF/Peter/State/Navigators/Navigator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using MovieDatabase.Domain;
+using MovieDatabase.WPF.Peter.Commands;
+using MovieDatabase.WPF.Peter.ViewModels;
+
+namespace MovieDatabase.WPF.Peter.State.Navigator
+{
+    public class Navigator : ObservableObject, INavigator
+    {
+        private BaseViewModel _currentViewModel;
+
+        public BaseViewModel CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged(nameof(CurrentViewModel));
+            }
+        }
+        public ICommand UpdateViewModelCommand => new UpdateViewModelCommand(this);
+    }
+}
